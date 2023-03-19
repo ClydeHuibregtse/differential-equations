a148b939999b6d5166c82dbd22ec0af4560c15dc
Clyde Huibregtse <clyde@huibregtse.com>
2023-03-19 12:48:07 -0700

    Implements Lotka Volterra solution with fwd Euler - experimenting with bkwd euler next for stability

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -1,4 +1,4 @@
-use ndarray::{arr0, concatenate, Array, Array1, Axis, s};
+use ndarray::{arr0, concatenate, Array, Array1, Axis, s, stack};
 use num::complex::Complex64;
 
 pub fn forward_euler<D>(
@@ -20,7 +20,7 @@ where
     let mut count = 0;
     while t < tf {
         let uf = &us[count];
-        println!("{:?}", uf);
+        // println!("{:?}", uf);
         us.push(uf + (h * &f(uf, t)));
         ts.push(t);
         t += h.re;
@@ -29,13 +29,15 @@ where
     (us, ts)
 }
 
-pub fn concatenate_vec_of_arrays<D>(us: Vec<Array<Complex64, D>>) -> Array<Complex64, D>
+pub fn concatenate_vec_of_arrays<D  >(us: Vec<Array<Complex64, D>>) -> Array<Complex64, D::Larger>
 where
     D: ndarray::Dimension + ndarray::RemoveAxis,
-{
-    let mut arr_us = us[0].clone();
-    for u in us[1..].iter() {
-        arr_us = concatenate(Axis(0), &[arr_us.view(), u.view()])
+{   
+    let shape = &us[0];
+    let mut arr_us = stack(Axis(0), &[us[0].view()]).unwrap();
+    for u in us[..].iter() {
+        u.view();
+        arr_us = concatenate(Axis(0), &[arr_us.view(), stack(Axis(0), &[u.view()]).unwrap().view()])
             .unwrap()
             .clone();
     }
@@ -46,14 +48,14 @@ where
 mod tests {
     use crate::plots::plot1d;
     use crate::solve::c_;
-    use ndarray::{concatenate, stack, Array, Array1, ArrayBase, Axis, Dim, OwnedRepr, ViewRepr};
+    use ndarray::{concatenate, stack, Array, Array1, ArrayBase, Axis, Dim, OwnedRepr, ViewRepr, s};
     use num::complex::Complex64;
 
     use super::{concatenate_vec_of_arrays, forward_euler};
 
     #[test]
     fn test_univariate_ode() {
-        let u0 = Array1::from_vec(vec![c_(0.0, 0.0)]);
+        let u0 = Array1::from_vec(vec![c_(1.0, 0.0)]);
         let t0 = 0.0;
         let tf = 6.28 * 2.0;
         let h = c_(0.1, 0.0);
@@ -64,6 +66,29 @@ mod tests {
             Array1::from_vec(ts),
             concatenate_vec_of_arrays(us),
             "test_plots/d_dx_cosx.png",
+        )
+        .unwrap();
+    }
+
+
+    #[test]
+    fn test_lotka_volterra_ode() {
+        let u0 = Array1::from_vec(vec![c_(10.0, 0.0), c_(10.0, 0.0)]);
+        let t0 = 0.0;
+        let tf = 30f64;
+        let h = c_(0.001, 0.0);
+        let (a, b, c, d) = (1.1f64, 0.4f64, 0.4f64, 0.1f64);
+        let f = |x: &Array1<Complex64>, t: f64| Array1::from_vec(
+            vec![a * x[0] - b * x[0] * x[1], -c * x[1] + d * x[0] * x[1]]
+        );
+
+        let (us, ts) = forward_euler(f, u0, t0, tf, h);
+        let cat_us = concatenate_vec_of_arrays(us);
+        println!{"{:?}", cat_us};
+        plot1d(
+            Array1::from_vec(ts),
+            cat_us,
+            "test_plots/lotka_volterra_fwd_euler.png",
         )
         .unwrap();
     }
--- src/plots.rs
+++ src/plots.rs
@@ -1,12 +1,20 @@
 use std::iter::zip;
 
-use ndarray::Array1;
+use ndarray::{Array1, Array2};
 use num::complex::Complex64;
 use plotters::prelude::*;
 
+static COLORS:&'static [&RGBColor; 5]= &[
+    &RED,
+    &GREEN,
+    &BLUE,
+    &YELLOW,
+    &CYAN,
+];
+
 pub fn plot1d(
     x: Array1<f64>,
-    y: Array1<Complex64>,
+    ys: Array2<Complex64>,
     fpath: &str,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let root = BitMapBackend::new(fpath, (640, 480)).into_drawing_area();
@@ -15,24 +23,26 @@ pub fn plot1d(
 
     let xmax = *x.iter().max_by(|x, y| x.partial_cmp(y).unwrap()).unwrap() as f32;
     let xmin = *x.iter().min_by(|x, y| x.partial_cmp(y).unwrap()).unwrap() as f32;
-    let ymax = y.iter().max_by(|x, y| x.norm().partial_cmp(&y.norm()).unwrap()).unwrap().re as f32;
-    let ymin = y.iter().min_by(|x, y| x.norm().partial_cmp(&y.norm()).unwrap()).unwrap().re as f32;
-    
+
     let mut chart = ChartBuilder::on(&root)
         .margin(5)
         .x_label_area_size(30)
         .y_label_area_size(30)
-        .build_cartesian_2d(xmin..xmax, -ymin..ymax)?;
-
+        // .build_cartesian_2d(xmin..xmax, ymin..ymax)?;
+        .build_cartesian_2d(xmin..xmax, 0f32..30f32)?;
     chart.configure_mesh().draw()?;
-
-    chart
-        .draw_series(LineSeries::new(
-            zip(x.map(|v| *v as f32), y.map(|v| v.re as f32)).into_iter(),
-            &RED,
-        ))?
-        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
-
+    for (y, color) in ys.columns().into_iter().zip(COLORS.iter()) {
+        // println!("{:?} {:?}", y, y.shape());
+        // let ymax = y.iter().max_by(|x, y| x.re.partial_cmp(&y.re).unwrap()).unwrap().re as f32;
+        // let ymin = y.iter().min_by(|x, y| x.re.partial_cmp(&y.re).unwrap()).unwrap().re as f32;
+        // println!("{:?} {:?}", ymin, ymax);
+        chart
+            .draw_series(LineSeries::new(
+                zip(x.map(|v| *v as f32), y.map(|v| v.re as f32)).into_iter(),
+                color,
+            ))?;
+            // .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], color));
+    }
     chart
         .configure_series_labels()
         .background_style(&WHITE.mix(0.8))
